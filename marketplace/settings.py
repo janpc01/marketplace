fb03bd2c401353fdb8de3ed9253253d544810d58
Jan <janpc01@gmail.com>
2022-11-02 08:48:58 -0600

    Added styling with mdbootstrap template

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -59,9 +59,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
