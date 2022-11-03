e98210b9512e0b5b555aa12d6d72ab77555ec013
janpc01 <janpc01@gmail.com>
2022-11-03 12:25:22 -0600

    Linked html files to base.html, navbar

--- core/models.py
+++ core/models.py
@@ -3,10 +3,24 @@ from django.db import models
 
 # Create your models here.
 
+CATEGORY_CHOICES = (
+    ('R', 'Regular'),
+    ('J', 'Jumbo'),
+    ('S', 'Special')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=CATEGORY_CHOICES, max_length=1)
 
     def __str__(self) -> str:
         return self.title
